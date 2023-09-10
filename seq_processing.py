6cccb2f49557b06d09d7fc3a44bbd5b1b5367848
Morgan Johansson <morgan.johansson@gmail.com>
2023-09-10 06:53:24 +0200

    Adding opencv as dependency.

--- seq_processing.py
+++ seq_processing.py
@@ -127,6 +127,29 @@ def _ffmpeg(config, filenames, fps, output):
         os.unlink(tempfilepath)
 
 
+class DreamVideoEncoderOpenCV:
+    NODE_NAME = "OpenCV Video Encoder"
+
+
+    def encode(self):
+        pass
+        """self._name = name + '.mp4'
+        self._cap = VideoCapture(0)
+        self._fourcc = VideoWriter_fourcc(*'MP4V')
+        self._out = VideoWriter(self._name, self._fourcc, 20.0, (640, 480))
+
+
+        from moviepy.editor import *
+
+        img = ['1.png', '2.png', '3.png', '4.png', '5.png', '6.png',
+               '7.png', '8.png', '9.png', '10.png', '11.png', '12.png']
+
+        clips = [ImageClip(m).set_duration(2)
+                 for m in img]
+
+        concat_clip = concatenate_videoclips(clips, method="compose")
+        concat_clip.write_videofile("test.mp4", fps=24)"""
+
 class DreamVideoEncoder:
     NODE_NAME = "FFMPEG Video Encoder"
     ICON = "🎬"
