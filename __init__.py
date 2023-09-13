4418ba94545d531328261a68be931e9d853536bd
Morgan Johansson <morgan.johansson@gmail.com>
2023-09-13 18:54:10 +0200

    version

--- __init__.py
+++ __init__.py
@@ -31,7 +31,7 @@ _SIGNATURE_SUFFIX = " [Dream]"
 
 MANIFEST = {
     "name": "Dream Project Animation",
-    "version": (3, 1, 0),
+    "version": (3, 1, 1),
     "author": "Dream Project",
     "project": "https://github.com/alt-key-project/comfyui-dream-project",
     "description": "Various utility nodes for creating animations with ComfyUI",
