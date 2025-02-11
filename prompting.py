96258138b666e31dece3894a994c6aa7f122c37e
Morgan Johansson <alt.key.project@gmail.com>
2025-02-11 20:30:11 +0100

    Re-added random prompt node.

--- prompting.py
+++ prompting.py
@@ -1,6 +1,50 @@
 from .categories import NodeCategories
 from .shared import hashed_as_strings
 from .dreamtypes import PartialPrompt
+import random
+
+class DreamRandomPromptWords:
+    NODE_NAME = "Random Prompt Words"
+    ICON = "⚅"
+
+    @classmethod
+    def INPUT_TYPES(cls):
+        return {
+            "optional": {
+                "partial_prompt": (PartialPrompt.ID,)
+            },
+            "required": {
+                "words": ("STRING", {"default": "", "multiline": True}),
+                "separator": ("STRING", {"default": ",", "multiline": False}),
+                "samples": ("INT", {"default": 1, "min": 1, "max": 100}),
+                "min_weight": ("FLOAT", {"default": 1.0, "min": -10, "max": 10}),
+                "max_weight": ("FLOAT", {"default": 1.0, "min": -10, "max": 10}),
+                "seed": ("INT", {"default": 0, "min": 0, "max": 0xffffffffffffffff}),
+            },
+        }
+
+    CATEGORY = NodeCategories.CONDITIONING
+    RETURN_TYPES = (PartialPrompt.ID,)
+    RETURN_NAMES = ("partial_prompt",)
+    FUNCTION = "result"
+
+
+    @classmethod
+    def IS_CHANGED(cls, *values, **kwargs):
+        return hashed_as_strings(*values, **kwargs)
+
+    def result(self, words: str, separator, samples, min_weight, max_weight, seed, **args):
+        p = args.get("partial_prompt", PartialPrompt())
+        rnd = random.Random()
+        rnd.seed(seed)
+        words = list(set(map(lambda s: s.strip(), filter(lambda s: s.strip() != "", words.split(separator)))))
+        samples = min(samples, len(words))
+        for i in range(samples):
+            picked_word = words[rnd.randint(0, len(words)-1)]
+            words = list(filter(lambda s: s!=picked_word, words))
+            weight = rnd.uniform(min_weight, max_weight)
+            p = p.add(picked_word, weight)
+        return (p,)
 
 
 class DreamWeightedPromptBuilder:
